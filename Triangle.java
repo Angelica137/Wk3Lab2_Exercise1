9d4b9b4f3fed9b7c471eb1c434b120990c361650
Angelica <Angelica137@users.noreply.github.com>
2021-03-25 22:21:50 +0000

    add colour and set scene

--- Triangle.java
+++ Triangle.java
@@ -3,6 +3,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
 
 public class Triangle extends Application {
 	@Override
@@ -11,8 +12,18 @@ public class Triangle extends Application {
 		// create the lines
 		Line line1, line2, line3;
 		line1 = new Line(100, 70, 400, 250);
+		line1.setStroke(Color.BLUE);
 		line2 = new Line(100, 70, 100, 250);
+		line2.setStroke(Color.BLUE);
 		line3 = new Line(100, 250, 400, 250);
+		line3.setStroke(Color.BLUE);
+
+		// create a group that holds th triangle
+		Group root = new Group(line1, line2, line3);
+
+		// create and config a scene
+		Scene scene = new Scene(root, 500, 500);
+
 	}
 
 }
